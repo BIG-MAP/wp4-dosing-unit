8d85c2f2dfe9584d3b86d11c132a775b8f4215f0
Ihar Suvorau <ihar.suvorau@gmail.com>
2023-08-02 09:40:18 +0200

    Fix the listing ports script

--- tests/list_serial_ports.py
+++ tests/list_serial_ports.py
@@ -1,4 +1,4 @@
 from serial.tools import list_ports
 
-for device in list_ports():
+for device in list_ports.comports():
     print(f"name={device.name}, description={device.description}, hwid={device.hwid}")
