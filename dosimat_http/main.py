8be5ed0937820b746cb5d8c7d718f700b0586c9e
Ihar Suvorau <ihar.suvorau@gmail.com>
2023-08-02 11:29:15 +0200

    Add more exceptions to HTTP server

--- dosimat_http/main.py
+++ dosimat_http/main.py
@@ -1,8 +1,9 @@
 import logging
 from dataclasses import dataclass
-from typing import Any, Optional
+from typing import Any, Optional, Union
 
 from fastapi import BackgroundTasks, FastAPI
+from fastapi.responses import JSONResponse
 
 from dosimat_http.dosimat_manager import DosimatManager
 
@@ -38,13 +39,16 @@ async def list_dosimats() -> APIResponse:
 
 
 @app.get("/dosimats/{id}/status")
-async def get_status(id: int) -> APIResponse:
+async def get_status(id: int) -> Union[APIResponse, JSONResponse]:
     """
     Returns the status of the dosing unit.
     """
     dosimat = app.state.dosimat_manager.get_unit(id)
     if dosimat is None:
-        return APIResponse(error=f"Could not find dosimat with id {id}")
+        return JSONResponse(
+            status_code=404,
+            content=APIResponse(error=f"Could not find dosimat with id {id}"),
+        )
 
     is_ready = dosimat.is_ready()
     return APIResponse(data={"is_ready": is_ready})
@@ -53,7 +57,7 @@ async def get_status(id: int) -> APIResponse:
 @app.post("/dosimats/{id}/dispense", status_code=202)
 async def dispense(
     id: int, ml: float, background_tasks: BackgroundTasks
-) -> APIResponse:
+) -> Union[APIResponse, JSONResponse]:
     """
     Dispenses the given volume of liquid.
     """
@@ -62,11 +66,28 @@ async def dispense(
     if ml.is_integer():
         ml = int(ml)
 
+    dosimat = app.state.dosimat_manager.get_unit(id)
+    if dosimat is None:
+        return JSONResponse(
+            status_code=404,
+            content=APIResponse(error=f"Could not find dosimat with id {id}"),
+        )
+
+    is_ready = dosimat.is_ready()
+    if not is_ready:
+        return JSONResponse(
+            status_code=400,
+            content=APIResponse(error=f"Dosimat with id {id} is not ready"),
+        )
+
     try:
         background_tasks.add_task(app.state.dosimat_manager.dispense, id, ml)
         return APIResponse(message="Dispensing successful")
     except Exception as e:
-        return APIResponse(error=str(e))
+        return JSONResponse(
+            status_code=500,
+            content=APIResponse(error=f"Could not dispense: {e}"),
+        )
 
 
 if __name__ == "__main__":
