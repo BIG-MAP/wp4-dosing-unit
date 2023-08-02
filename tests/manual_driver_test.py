cb34da25671cb71b0a989cac77d7d9a87009faf9
Ihar Suvorau <ihar.suvorau@gmail.com>
2023-08-02 09:36:36 +0200

    Update the manual test

--- tests/manual_driver_test.py
+++ tests/manual_driver_test.py
@@ -1,7 +1,11 @@
+import logging
+
 from dosimat_driver.driver import Dosimat876
+
+logging.basicConfig(level=logging.INFO)
 
 serial_port = "/dev/ttyUSB0"
 
 dosimat = Dosimat876(serial_port)
 
-dosimat.dispense(1.0)
+dosimat.dispense(10)
